eaf77d3515c6899571c5526e94f7839cced52ba1
ivandevp <medinadiazivan@gmail.com>
2018-07-18 16:28:59 -0500

    changes webpack config by rollup

---
+++ rollup.config.babel.js
@@ -0,0 +1,31 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+    input: './src/index.js',
+    moduleName: 'LaboratoriaUI',
+    sourcemap: true,
+    targets: [
+        {
+            dest: './dist/laboratoria-ui.js',
+            format: 'umd'
+        },
+        {
+            dest: './dist/laboratoria-ui.module.js',
+            format: 'es'
+        }
+    ],
+    plugins: [
+        babel({
+            exclude: './node_modules/**'
+        }),
+        resolve(),
+        commonjs()
+    ],
+    external: ['react', 'react-dom'],
+    globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM'
+    }
+};
