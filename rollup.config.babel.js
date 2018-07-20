e0e32a0da8e809fce078f090553a7eb5dc835da2
Guilherme Moura <gmoura.nas@gmail.com>
2018-07-20 18:40:12 -0500

    Try to fix

--- rollup.config.babel.js
+++ rollup.config.babel.js
@@ -30,6 +30,7 @@ export default {
     external: ['react', 'react-dom'],
     globals: {
         react: 'React',
-        'react-dom': 'ReactDOM'
+        'react-dom': 'ReactDOM',
+        document: document
     }
 };
