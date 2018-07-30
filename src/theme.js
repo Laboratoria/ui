ff571af5348892e38e3b1187c1f9c7908c727238
Guilherme Moura <gmoura.nas@gmail.com>
2018-07-30 09:17:06 -0300

    Add <h1>, <h2> and <button> style

--- src/theme.js
+++ src/theme.js
@@ -3,8 +3,25 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   typography: {
     fontFamily: [
-      'Bitter',
+      'Open Sans,sans-serif',
     ],
+    title: {
+      fontFamily: 'Bitter,serif',
+      fontWeight: '700',
+      lineHeight: '100%'
+    },
+    subheading: {
+      fontFamily: 'Bitter,serif',
+      fontWeight: '700',
+      lineHeight: '100%'
+    },
+    button: {
+      fontSize: "16px",
+      textTransform: "uppercase",
+      fontWeight: 900,
+      fontFamily: 'Open Sans,sans-serif',
+      color: "rgba(0, 0, 0, 0.87)",
+    }
   },
   palette: {
     primary: {
