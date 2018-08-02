f2bd1e6af7c3360c7bdc5e65cf0ff5124dd6d67c
Guilherme Moura <gmoura.nas@gmail.com>
2018-08-02 18:51:17 -0300

    Fidex tabs and imports

--- src/components/MainTitle.js
+++ src/components/MainTitle.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/core/styles/withStyles';
+import { Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
 const style = {
   title: {
