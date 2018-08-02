3cfa8d4dee027ba953d568f70b64a5df420147fc
Guilherme Moura <gmoura.nas@gmail.com>
2018-08-02 17:00:40 -0300

    Update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import nodeGlobals from 'rollup-plugin-node-globals';
 
-const input = './src/index.js';
+const input = './src/components/index.js';
 const name = 'laboratoria-ui';
 const globals = {
   react: 'React',
