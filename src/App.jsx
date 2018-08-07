93a32f714d4aa65f99fa3082c6ac4d745f96448f
RuthMeryCardenas <mery.cardenas.perez@gmail.com>
2018-08-07 16:48:33 -0500

    Fixes the invocation of the divider component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
