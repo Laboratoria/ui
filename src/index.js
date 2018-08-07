a60244f1e1831834114880288afc33c63b322885
Guilherme Moura Nascimento <gmoura.nas@gmail.com>
2018-08-07 15:36:39 -0300

    Visual examples (#14)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import Theme from './Theme';
-import Button from './Button';
 
-export default {
-  Theme,
-  Button,
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Inject a theme into your application
+import { MuiThemeProvider } from '@material-ui/core/styles';
+// Our theme
+import Theme from './components/Theme';
+import App from './App';
+
+ReactDOM.render(
+  <MuiThemeProvider theme={Theme}>
+    <App />
+  </MuiThemeProvider>,
+
+  document.getElementById('root'),
+);
