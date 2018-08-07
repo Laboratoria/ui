486cf95a1285ef655e0c53d0af48c379ae6cefe5
Guilherme Moura <gmoura.nas@gmail.com>
2018-08-07 15:27:04 -0300

    Add new lines

--- src/components/Titles/MainTitle.jsx
+++ src/components/Titles/MainTitle.jsx
@@ -2,23 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
- const style = {
+
+const style = {
   title: {
     marginTop: 10,
     color: 'blur',
     fontSize: 24,
   },
 };
- const MainTitle = (props) => {
+
+const MainTitle = (props) => {
   const { classes, children } = props;
+  
   return (
     <Typography className={classes.title} variant="headline" gutterBottom>
       {children}
     </Typography>
   );
 };
- MainTitle.propTypes = {
+
+MainTitle.propTypes = {
   classes: PropTypes.shape().isRequired,
   children: PropTypes.string.isRequired,
 };
- export default withStyles(style)(MainTitle);
+
+export default withStyles(style)(MainTitle);
