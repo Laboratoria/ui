3b7ef76970d052a403edea6d5640e044f9eb2535
Guilherme Moura <gmoura.nas@gmail.com>
2018-08-07 17:34:58 -0300

    Fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
 
 const styles = {
   root: {
