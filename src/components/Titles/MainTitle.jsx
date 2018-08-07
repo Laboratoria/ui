ea36bfd56c40e71ed12d58f9f167e33ba598fc27
Guilherme Moura Nascimento <gmoura.nas@gmail.com>
2018-08-07 15:28:38 -0300

    Main Title Component (#11)
    * Create the MainTitle Component
    
    * Add new lines

---
+++ src/components/Titles/MainTitle.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+const style = {
+  title: {
+    marginTop: 10,
+    color: 'blur',
+    fontSize: 24,
+  },
+};
+
+const MainTitle = (props) => {
+  const { classes, children } = props;
+  
+  return (
+    <Typography className={classes.title} variant="headline" gutterBottom>
+      {children}
+    </Typography>
+  );
+};
+
+MainTitle.propTypes = {
+  classes: PropTypes.shape().isRequired,
+  children: PropTypes.string.isRequired,
+};
+
+export default withStyles(style)(MainTitle);
