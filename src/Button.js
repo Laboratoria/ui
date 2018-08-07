2c96045bc4de171ddd5c56075abd3fb9584d1be1
Guilherme Moura <gmoura.nas@gmail.com>
2018-08-07 11:13:06 -0300

    Remove Button

--- src/Button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
-
-const LabButton = (props) => (
-    <Button color="secondary">{props.children}</Button>
-);
-
-export default LabButton;
