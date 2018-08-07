9b85010f9a07ea036fe2471cc58e5c86c36419f8
Guilherme Moura <gmoura.nas@gmail.com>
2018-08-07 13:53:21 -0300

    Visual examples

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  root: {
+    textAlign: 'center',
+  },
+};
+
+const App = (props) => {
+  const { classes } = props;
+  return (
+    <React.Fragment>
+      <Grid container spacing={24} className={classes.root}>
+        <Grid item xs={12}>
+          <Typography variant="display3" gutterBottom>
+          Laboratoria-ui Styleguide
+          </Typography>
+          <Divider className={classes.dividers} />
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
+};
+
+App.propTypes = {
+  classes: PropTypes.shape().isRequired,
+};
+
+export default withStyles(styles)(App);
