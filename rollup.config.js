290f2b71f2eb345634f013c76dd75ea6a10cdded
Guilherme Moura Nascimento <gmoura.nas@gmail.com>
2018-08-07 15:23:36 -0300

    Bundle fixed - Rollup (#9)
    * Remove old builds
    
    * Fixed material-ui bugs

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,18 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import nodeGlobals from 'rollup-plugin-node-globals';
+import jsx from 'rollup-plugin-jsx';
 
-const input = './src/index.js';
+const input = './src/components/index.jsx';
 const name = 'laboratoria-ui';
 const globals = {
   react: 'React',
   'react-dom': 'ReactDOM',
+  '@material-ui/core/styles': '@material-ui/core/styles',
+  '@material-ui/core': '@material-ui/core',
 };
 const babelOptions = {
-    exclude: './node_modules/**'
+  exclude: './node_modules/**',
 };
 const commonjsOptions = {
   ignoreGlobal: true,
@@ -20,24 +23,21 @@ const commonjsOptions = {
 export default [
   {
     input,
-    output: { file: `build/umd/${name}.development.js`, format: 'umd', name, globals },
+    output: {
+      file: `dist/umd/${name}.production.min.js`,
+      format: 'umd',
+      name,
+      globals,
+    },
     external: Object.keys(globals),
     plugins: [
-      nodeResolve(),
+      nodeResolve({
+        extensions: ['.js', '.jsx'],
+      }),
       babel(babelOptions),
-      commonjs(),
+      commonjs(commonjsOptions),
       nodeGlobals(),
-    ],
-  },
-  {
-    input,
-    output: { file: `build/umd/${name}.production.min.js`, format: 'umd', name, globals },
-    external: Object.keys(globals),
-    plugins: [
-      nodeResolve(),
-      babel(babelOptions),
-      commonjs(),
-      nodeGlobals(),
+      jsx({ factory: 'React.createElement' }),
     ],
   },
 
