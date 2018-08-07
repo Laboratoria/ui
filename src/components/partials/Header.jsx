ef9b42bb93c31f92e43071b334be36d943df2b9a
Guilherme Moura Nascimento <gmoura.nas@gmail.com>
2018-08-07 15:30:59 -0300

    Header Component (#12)
    * Header Component
    
    * Add new lines

---
+++ src/components/partials/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+
+const Header = (props) => {
+  const { classes, image } = props;
+   return (
+    <Grid
+      container
+      component="header"
+      className={classes}
+    >
+      <Grid item xs={3} md={4} />
+      <Grid
+        item
+        xs={6}
+        md={4}
+        container
+        display="flex"
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <img src={image} alt="Laboratoria" />
+      </Grid>
+      <Grid item xs={3} md={4} />
+    </Grid>
+  );
+};
+
+Header.propTypes = {
+  classes: PropTypes.shape().isRequired,
+  image: PropTypes.string.isRequired,
+};
+
+export default Header;
