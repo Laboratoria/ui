d6232d6133b192b8384277d618ab28e7eff3702f
RuthMeryCardenas <mery.cardenas.perez@gmail.com>
2018-08-08 12:31:25 -0500

    Adds header in App

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
+import Header from './components/partials/Header';
 
 const styles = {
   root: {
     textAlign: 'center',
+  },
+  header: {
+    height: 60,
   },
 };
 
@@ -15,14 +19,15 @@ const App = (props) => {
   const { classes } = props;
   return (
     <React.Fragment>
+      <Header image="img/Laboratoria_logo.svg" className={classes.header} /> 
       <Grid container spacing={24} className={classes.root}>
-        <Grid item xs={12}>
-          <Typography variant="display3" gutterBottom>
-          Laboratoria-ui Styleguide
-          </Typography>
-          <Divider className={classes.dividers} />
+          <Grid item xs={12}>
+            <Typography variant="display3" gutterBottom>
+              Laboratoria-ui Styleguide
+            </Typography>
+            <Divider className={classes.dividers} />
+          </Grid>
         </Grid>
-      </Grid>
     </React.Fragment>
   );
 };
