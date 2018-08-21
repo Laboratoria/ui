8eabfa8b01e44022405e3d30de3055f1425b2373
Mery Cardenas <mery.cardenas.perez@gmail.com>
2018-08-21 13:41:50 -0500

    feature(typography): updates typography on theme (#20)
    * Updates typography
    
    * Texts Component
    
    * Updates Family Font on visual page
    
    * Updates CSS styles of Colors component
    
    * Add Text component on visual page
    
    * Updates typography

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,14 @@ import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Header from './components/partials/Header';
 import Colors from './components/brand/Colors';
+import Texts from './components/brand/Texts';
+
 
 const styles = {
   'main-content': {
-    textAlign: 'center',
+    margin: '0 auto',
+    maxWidth: 1200,
+    width: '90%',
   },
   header: {
     height: 60,
@@ -21,36 +25,72 @@ const App = (props) => {
   return (
     <React.Fragment>
       <Header image="img/Laboratoria_logo.svg" className={classes.header} /> 
-      <Grid container spacing={24} className={classes['main-content']}>
+      <Grid container className={classes['main-content']}>
         <Grid item xs={12}>
           <Grid container>
             <Grid item xs={12}>
-              <Typography variant="display3" gutterBottom>
-                  Laboratoria-ui Styleguide
+              <Typography variant="display4" gutterBottom align="center" component="h1">
+                Laboratoria-ui Styleguide
               </Typography>
-              <Divider className={classes.dividers} />
             </Grid>
           </Grid>
         </Grid>
         <Grid container>
           <Grid item xs={12}>
-            <Typography variant="display2" gutterBottom>
-                Brand Colors
+            <Typography variant="display3" gutterBottom component="h2" >
+              Brand Colors
+            </Typography>
+            <Typography gutterBottom>
+              Variables disponibles para los colores.
+            </Typography>
+            <Typography variant="display2" gutterBottom component="h3" >
+              Spot Colors
+            </Typography>
+            <Typography variant="display1" gutterBottom component="h4" >
+              Variables
+            </Typography>
+            <Typography gutterBottom>
+              A continuación una descripción de las variables y los colores que representan.
             </Typography>
           </Grid>
           <Grid item xs={12}>
-            <Typography variant="display1" gutterBottom>
+            <Typography variant="title" gutterBottom>
               Yellow
             </Typography>
             <Colors color="primary" />
-            <Typography variant="display1" gutterBottom>
+            <Typography variant="title" gutterBottom>
               Magenta
             </Typography>
             <Colors color="magenta" />
-            <Typography variant="display1" gutterBottom>
+            <Typography variant="title" gutterBottom>
               Mint
             </Typography>
             <Colors color="mint" />
+            <Divider className={classes.dividers} />
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={12}>
+            <Typography variant="display3" component="h2" gutterBottom>
+              Typography
+            </Typography>
+            <Typography gutterBottom>
+              A continuación una lista de los tipos de tipografía:
+            </Typography>
+            <Typography variant="display2" component="h3" gutterBottom>
+              Font Types
+            </Typography>
+            <Typography gutterBottom>
+              Hay 2 tipos de tipografías
+            </Typography>
+            <ul>
+              <li>Bitter</li>
+              <li>Open Sans</li>
+            </ul>
+            <Typography gutterBottom>
+              h1, h2, h3, h4, h5, h6 por default siempre serán BITTER
+            </Typography>
+            <Texts />
             <Divider className={classes.dividers} />
           </Grid>
         </Grid>
--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -1,28 +1,6 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
-  typography: {
-    fontFamily: [
-      'Open Sans,sans-serif',
-    ],
-    title: {
-      fontFamily: 'Bitter,serif',
-      fontWeight: '700',
-      lineHeight: '100%',
-    },
-    subheading: {
-      fontFamily: 'Bitter,serif',
-      fontWeight: '700',
-      lineHeight: '100%',
-    },
-    button: {
-      fontSize: '16px',
-      textTransform: 'uppercase',
-      fontWeight: 900,
-      fontFamily: 'Open Sans,sans-serif',
-      color: 'rgba(0, 0, 0, 0.87)',
-    },
-  },
   palette: {
     primary: {
       light: '#ffea4d',
@@ -38,6 +16,54 @@ const theme = createMuiTheme({
       light: '#77f9ae',
       main: '#56f89a',
       dark: '#3cad6b',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Open Sans,sans-serif',
+    ],
+    display4: {
+      color: "#000",
+      fontFamily: 'Bitter,serif',
+      fontSize: '2.5rem',
+    },
+    display3: {
+      color: "#000",
+      fontFamily: 'Bitter,serif',
+      fontSize: '1.8rem',
+    },
+    display2: {
+      color: "#000",
+      fontFamily: 'Bitter,serif',
+      fontSize: '1.4rem',
+    },
+    display1: {
+      color: "#000",
+      fontFamily: 'Bitter,serif',
+      fontSize: '1.2rem',
+    },
+    headline: {
+      fontFamily: 'Bitter,serif',
+      fontSize: '1rem',
+    },
+    title: {
+      fontFamily: 'Bitter,serif',
+      fontSize: '0.8rem',
+    },
+    subheading: {
+      fontFamily: 'Bitter,serif',
+      fontSize: '1rem',
+      fontWeight: '600',
+    },
+    body1: {
+      fontSize: '1rem',
+    },
+    body2: {
+      fontSize: '0.8rem',
+    },
+    button: {
+      fontSize: '0.8rem',     
+      fontWeight: 800,
     },
   },
 });
