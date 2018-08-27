19a385a8f309fef2706d0c44b6c4403cf4443bf0
Guilherme Moura <gmoura.nas@gmail.com>
2018-08-27 16:40:39 -0300

    refactor(App): update paths and components names

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Divider from '@material-ui/core/Divider';
 import { withStyles } from '@material-ui/core/styles';
-import PrimaryButton from './components/Buttons';
-import CustomTypography from './components/partials/CustomTypography';
+import { TypographyDisplay6 } from './components/Typography';
 import Header from './components/partials/Header';
-import Colors from './components/brand/Colors';
-import Font from './components/brand/Font';
+import Colors from './how-to-use/Brand/Colors';
+import Font from './how-to-use/Brand/Font';
+import Buttons from './how-to-use/Buttons';
 
 const styles = {
   container: {
@@ -23,21 +23,14 @@ const App = ({ classes }) => (
   <React.Fragment>
     <Header image="img/Laboratoria_logo.svg" className={classes.header} />
     <div className={classes.container}>
-      <CustomTypography variant="display6" gutterBottom align="center" component="h1">
+      <TypographyDisplay6 gutterBottom align="center" component="h1">
         Laboratoria-ui Styleguide
-      </CustomTypography>
+      </TypographyDisplay6>
       <Colors />
       <Divider />
       <Font />
       <Divider />
-      <section>
-        <CustomTypography variant="display5" component="h2" gutterBottom>
-          Buttons
-        </CustomTypography>
-        <PrimaryButton tag="a">
-          Publica una posición
-        </PrimaryButton>
-      </section>
+      <Buttons />
     </div>
   </React.Fragment>
 );
