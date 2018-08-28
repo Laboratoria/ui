4017514aea2c1be4c310ddb656d823a3ea45598f
Guilherme Moura <gmoura.nas@gmail.com>
2018-08-28 19:45:25 -0300

    feat(flags): add svg flags
    * peru
    * brazil
    * mexico
    * chile

---
+++ src/components/SVG/flags/index.jsx
@@ -0,0 +1,6 @@
+export { default as aqp } from './peru.svg'; // Arequipa
+export { default as lim } from './peru.svg'; // Lima
+export { default as gdl } from './mexico.svg'; // Guadalajara
+export { default as cdmx } from './mexico.svg'; // Ciudad de mexico
+export { default as scl } from './chile.svg'; // Santiago del chile
+export { default as sp } from './brazil.svg'; // São Paulo
