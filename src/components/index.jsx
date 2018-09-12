27e0111a487badce80cb09e172b864a23703a8fa
Guilherme Moura <gmoura.nas@gmail.com>
2018-09-12 15:59:10 -0300

    refactor(svg): add svg flags on main exports

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,3 +3,4 @@ export { default as Buttons } from './Buttons';
 export { default as Header } from './partials/Header';
 export { TypographyDisplay5, TypographyDisplay6 } from './Typography';
 export { default as CardMediaGitHub } from './Cards';
+export * from './SVG/flags';
