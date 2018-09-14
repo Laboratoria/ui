dfd27d1fbf8e6a0d08f7489c15123a1197e1e2f6
Guilherme Moura <gmoura.nas@gmail.com>
2018-09-14 14:13:56 -0300

    docs(examples): update examples with CardMediaGitHub

--- src/how-to-use/Cards/index.jsx
+++ src/how-to-use/Cards/index.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import CardMedia from '@material-ui/core/CardMedia';
 import CardMediaGitHub from '../../components/Cards';
 import { TypographyDisplay5 } from '../../components/Typography';
+import {
+  aqp,
+  lim,
+  gdl,
+  cdmx,
+  scl,
+  sp,
+} from '../../components/SVG/flags';
+
+const Flags = {
+  aqp,
+  lim,
+  gdl,
+  cdmx,
+  scl,
+  sp,
+};
 
 const graduated = {
   uid: 'JfStPqpcrfdh97RR2OUOUtmZqDl2',
@@ -23,21 +43,83 @@ const graduated = {
   role: 'student',
 };
 
-const GraduatedCard = () => {
+const styles = {
+  root: {
+    display: 'block',
+    width: '100%',
+    height: '100%',
+    textDecoration: 'none',
+    color: 'inherit',
+  },
+  media: {
+    width: '100%',
+    height: '100%',
+  },
+  flag: {
+    width: '20px',
+    position: 'absolute',
+    bottom: '7px',
+    right: '7px',
+  },
+};
+
+const GraduatedCard = ({ classes }) => {
+  const Thumbnail = ({ to, data }) => (
+    <a href={to} className={classes.root}>
+      <CardMedia image={`https://github.com/${data.github}.png?size=300`} title={data.name} className={classes.media}>
+        <img src={Flags[data.campus]} alt={data.campus} className={classes.flag} />
+      </CardMedia>
+    </a>
+  );
+
+  Thumbnail.propTypes = {
+    to: PropTypes.string.isRequired,
+    data: PropTypes.shape().isRequired,
+  };
+
+  const Title = ({ text, to }) => (
+    <a href={to} className={classes.root}>{text}</a>
+  );
+
+  Title.propTypes = {
+    text: PropTypes.string.isRequired,
+    to: PropTypes.string.isRequired,
+  };
+
+  const Subtitle = ({ text, to }) => (
+    <a href={to} className={classes.root}>{text}</a>
+  );
+
+  Subtitle.propTypes = {
+    text: PropTypes.string.isRequired,
+    to: PropTypes.string.isRequired,
+  };
+
   const action = (
     <Button variant="outlined" color="primary">
       Pre-seleccionar
     </Button>
-  )
+  );
+
   return (
     <section>
       <TypographyDisplay5 component="h2" gutterBottom>
         Graduated Card
       </TypographyDisplay5>
 
-      <CardMediaGitHub data={graduated} action={action} />
+      <CardMediaGitHub
+        data={graduated}
+        thumbnail={<Thumbnail data={graduated} to="https://app.talento.laboratoria.la/profile/MF0tL4sk8HcBQ0YHLxb0E8D5cD12" />}
+        title={<Title text={graduated.name} to="https://app.talento.laboratoria.la/profile/MF0tL4sk8HcBQ0YHLxb0E8D5cD12" />}
+        subtitle={<Subtitle text={graduated.recomendedAs} to="https://app.talento.laboratoria.la/profile/MF0tL4sk8HcBQ0YHLxb0E8D5cD12" />}
+        action={action}
+      />
     </section>
-  )
+  );
 };
 
-export default GraduatedCard;
+GraduatedCard.propTypes = {
+  classes: PropTypes.shape().isRequired,
+};
+
+export default withStyles(styles)(GraduatedCard);
