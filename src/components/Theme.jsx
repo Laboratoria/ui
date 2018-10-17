8ddc8aa50889e247aa17e87f39cddb2c25fb3cfd
RuthMeryCardenas <mery.cardenas.perez@gmail.com>
2018-10-17 11:03:52 -0500

    feat(theme): add CSS styles for headings h1,h2,h3,h4,h5 and h6

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -19,56 +19,70 @@ const theme = createMuiTheme({
     },
   },
   typography: {
+    useNextVariants: true,
     fontFamily: 'Open Sans, Arial, sans-serif',
     fontWeightMedium: 600,
-    display6: {
+    h1: {
       color: '#000',
       fontFamily: 'Bitter,serif',
       fontSize: '2.5rem',
-      fontWeight: 400,
+      fontWeight: 700,
+      letterSpacing: 'normal',
+      lineHeight: 'normal',
     },
-    display5: {
+    h2: {
       color: '#000',
       fontFamily: 'Bitter,serif',
       fontSize: '1.8rem',
-      fontWeight: 400,
+      fontWeight: 700,
+      letterSpacing: 'normal',
+      lineHeight: 'normal',
     },
-    display4: {
+    h3: {
       color: '#000',
       fontFamily: 'Bitter,serif',
       fontSize: '1.4rem',
+      fontWeight: 700,
       letterSpacing: 'normal',
       lineHeight: 'normal',
     },
-    display3: {
+    h4: {
       color: '#000',
       fontFamily: 'Bitter,serif',
       fontSize: '1.2rem',
+      fontWeight: 700,
       letterSpacing: 'normal',
       lineHeight: 'normal',
     },
-    display2: {
+    h5: {
       color: '#000',
       fontFamily: 'Bitter,serif',
       fontSize: '1rem',
+      fontWeight: 700,
+      letterSpacing: 'normal',
       lineHeight: 'normal',
     },
-    display1: {
+    h6: {
       color: '#000',
       fontFamily: 'Bitter,serif',
       fontSize: '0.8rem',
+      fontWeight: 700,
+      letterSpacing: 'normal',
       lineHeight: 'normal',
+    },
+    body1: {
+      color: '#000',
+      fontSize: '0.8rem',
+      fontWeight: 300,
+      letterSpacing: 'normal',
+      lineHeight: '1.35em',
     },
     body2: {
       color: '#000',
       fontSize: '1rem',
-      fontWeight: 600,
-      lineHeight: '1.3em',
-    },
-    body1: {
-      color: '#000',
-      fontSize: '1rem',
-      lineHeight: '1.3em',
+      fontWeight: 300,
+      letterSpacing: 'normal',
+      lineHeight: '1.35em',
     },
   },
   spacing: {
