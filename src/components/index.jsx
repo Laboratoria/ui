43a8d034e266cc0030c82ed898c1901587fce7e8
RuthMeryCardenas <mery.cardenas.perez@gmail.com>
2018-10-17 11:34:05 -0500

    refact(component): delete the export of Typography customized components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,6 +2,5 @@ export { default as Theme } from './Theme';
 export { default as Button } from './Buttons';
 export { default as Link } from './Links';
 export { default as Header } from './partials/Header';
-export { TypographyDisplay5, TypographyDisplay6 } from './Typography';
 export { default as CardMediaGitHub } from './Cards';
 export * from './SVG/flags';
