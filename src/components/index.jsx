2cbd1ec3903fd3e718ffa1241948897445afdfd4
Rafael Cerri <rafaelbcerri@gmail.com>
2019-04-23 17:01:41 -0300

    Export new component SearchBar

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,6 +3,7 @@ export { default as Button } from './Buttons';
 export { default as Link } from './Links';
 export { default as Header } from './partials/Header';
 export { default as CardMediaGitHub } from './Cards';
-export { default as SearchBar } from './SearchBar';
+export { default as SearchBarUI } from './SearchBar';
+export { default as SearchBar } from './SearchBar/searchBar';
 export * from './SVG/flags';
 export * from './SVG/icons';
