c0ccc0211640d30ae315681ae39c5f0dbcabf40a
Rafael Cerri <rafaelbcerri@hotmail.com>
2019-04-24 10:33:54 -0300

    refactor(SearchBar): Make component more general (#59)
    * fix(SearchBar): Send event instead of value
    
    * refactor(SearchBar): Make component more general
    
    - Remove statefull methods
    - Change component to be stateless
    - Remove debounce
    
    * chore(package): Remove debounce
    
    * Add storybook console and debouce packages
    
    * Rename component to SearchBarUI
    
    * Create a statefull SearchBar to use one placement
    
    * Export new component SearchBar
    
    * Create stories for new component
    
    * Create SearchBarModal component

---
+++ src/components/SearchBar/searchBar.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { debounce } from 'throttle-debounce';
+
+import SearchBarUI from './';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      showOptions: false
+    };
+
+    const { debounceTime, debounceCallback } = props;
+    this.debounce = debounce(debounceTime, debounceCallback);
+    
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    event.stopPropagation();
+    const { target: { value } } = event;
+    const { onChange } = this.props;
+    this.setState({ 
+      value,
+      showOptions: true
+    });
+    onChange(event);
+    this.debounce();
+  }
+
+  handleSelectedValue = (event) => {
+    event.stopPropagation();
+    const { onSelectValue } = this.props;
+    this.setState({
+      value: '',
+      showOptions: false,
+    });
+    onSelectValue(event);
+  }
+
+  render() {
+    const { inputProps, placeholder, options } = this.props;
+    const { value, showOptions } = this.state;
+
+    return (
+      <SearchBarUI
+        placeholder={placeholder}
+        options={options}
+        onChange={this.handleChange}
+        onSelectValue={this.handleSelectedValue}
+        inputProps={{...inputProps}}
+        value={value}
+        showOptions={showOptions}
+      />
+    );
+  }
+}
+
+SearchBar.defaultProps = {
+  debounceTime: 700
+};
+
+SearchBar.propTypes = {
+  debounceTime: PropTypes.number,
+  debounceCallback: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onSelectValue: PropTypes.func.isRequired,
+  onChange: PropTypes.func,
+  inputProps: PropTypes.object,
+};
+
+export default SearchBar;
