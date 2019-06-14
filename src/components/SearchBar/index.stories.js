c9ceb5c7bad1ae7def3f24bdc42223a2f6afd8f4
Ana Steph <anastephany.salazarguzman@gmail.com>
2019-06-14 11:13:29 -0500

    chore(refact): fix eslint errors and Typos (#68)
    * refac(eslint): fix eslint errors
    
    * fix typos

--- src/components/SearchBar/index.stories.js
+++ src/components/SearchBar/index.stories.js
@@ -2,25 +2,31 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import SearchBarUI  from './';
-import SearchBar  from './searchBar';
+import SearchBarUI from '.';
+import SearchBar from './searchBar';
 
 storiesOf('SearchBarUI', module)
   .add('searchBarUI closed', () => (
-    <SearchBarUI options={['Andrea', 'Guilherme', 'Dení', 'Rafael', 'Cesar']}
-      onSelectValue={action("onSelectValue")}
+    <SearchBarUI
+      options={['Andrea', 'Guilherme', 'Dení', 'Rafael', 'Cesar']}
+      onSelectValue={action('onSelectValue')}
       onChange={action('onChange')}
-      value={'Example'} />
+      value="Example"
+    />
   ))
   .add('searchBarUI opened', () => (
-    <SearchBarUI options={['Andrea', 'Guilherme', 'Dení', 'Rafael', 'Cesar']}
-      onSelectValue={action("onSelectValue")}
+    <SearchBarUI
+      options={['Andrea', 'Guilherme', 'Dení', 'Rafael', 'Cesar']}
+      onSelectValue={action('onSelectValue')}
       onChange={action('onChange')}
-      value={'Example'}
-      showOptions={true} />
+      value="Example"
+      showOptions
+    />
   ))
   .add('SearchBar', () => (
-    <SearchBar options={['Andrea', 'Guilherme', 'Dení', 'Rafael', 'Cesar']}
-      debounceCallback={action("debounceCallback")}
-      onSelectValue={action("onSelectValue")} />
+    <SearchBar
+      options={['Andrea', 'Guilherme', 'Dení', 'Rafael', 'Cesar']}
+      debounceCallback={action('debounceCallback')}
+      onSelectValue={action('onelectValue')}
+    />
   ));
