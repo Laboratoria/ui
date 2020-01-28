70e4fc874253fabd7b88fc53bbac9b317d2bdcf1
RuthMeryCardenas <mery.cardenas.perez@gmail.com>
2020-01-28 17:40:21 -0500

    refactor(input): update CSS styles

--- src/components/Inputs/style.js
+++ src/components/Inputs/style.js
@@ -3,18 +3,14 @@ export default {
     borderRadius: 2,
     boxSizing: 'border-box',
     display: 'flex',
-    marginBottom: 20,
+    marginBottom: 16,
   },
   label: {
     color: '#000',
-    '& + [class*="MuiInput-formControl"]': {
-      marginTop: 20,
-    },
   },
   input: {
     background: '#f7f7f7',
     color: '#000',
-    fontSize: 20,
-    padding: 16,
+    padding: '8px 16px',
   },
 };
