b9f88ed3c1eaad3301e8ffaf67c73273c58f41e6
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2020-02-12 18:59:40 -0500

    update(SideBar): Delete <main>

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -242,20 +242,6 @@ const SideBar = (props, theme) => {
           </div> 
         </List>
       </Drawer>
-      <main className={props.classes.content}>
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-        </Typography>
-      </main>
     </div>
   );
 }
@@ -303,6 +289,8 @@ SideBar.propTypes = {
 
 SideBar.defaultProps = {
   profile: {},
+  itemsArray: [],
+  iconsArray: [],
 };
 
 export default withStyles(styles)(SideBar);
