d770d85c56a337909febe3c24108e7948abd4fb3
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2020-02-12 17:17:58 -0500

    fix(storybook): Update Sidebar component

--- src/components/SideBar/index.stories.js
+++ src/components/SideBar/index.stories.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import HomeIcon from '@material-ui/icons/Home';
+import GroupIcon from '@material-ui/icons/Group';
 import { storiesOf } from '@storybook/react';
 
 import SideBarReadme from './README.md';
@@ -13,6 +15,14 @@ storiesOf('SideBar', module)
   })
   .add('SideBar', () => (
     <>
-      <SideBar />
+      <SideBar
+        items={[
+          { id: 'home', text: 'Inicio', icon: <HomeIcon /> },
+          { id: 'profiles', text: 'Perfiles', icon: <GroupIcon /> },
+        ]}
+        // eslint-disable-next-line no-console
+        logout={() => console.log('Logout')}
+        user={{ name: 'Nombre Apellidos', email: 'nombre.apellido@dominio.la' }}
+      />
     </>
   ));
