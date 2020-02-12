8065338bf789f81b7b4113381a6a84d90e78aeb0
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2020-02-12 17:11:15 -0500

    fix(sidebar): Update mobile version layout

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,299 +1,358 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import clsx from 'clsx';
 import withStyles from '@material-ui/core/styles/withStyles';
-import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import PersonIcon from '@material-ui/icons/Person';
-import DescriptionIcon from '@material-ui/icons/Description';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
 import { logoWhite, isotypeWhite } from '../SVG/logos';
 
-const drawerWidth = '25%';
+const drawerWidth = 300;
 
 const styles = theme => ({
-  active: {
-    opacity: 1,
-  },
   appBar: {
     backgroundColor: 'transparent',
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
+    boxShadow: 'none',
+    [theme.breakpoints.only('xs')]: {
+      zIndex: theme.zIndex.drawer + 1,
+    },
+    [theme.breakpoints.up('sm')]: {
+      transition: theme.transitions.create(['width', 'margin'], {
+        duration: theme.transitions.duration.leavingScreen,
+        easing: theme.transitions.easing.sharp,
+      }),
+    },
   },
   appBarShift: {
     backgroundColor: 'transparent',
-    width: `calc(100% - ${drawerWidth})`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(['width', 'margin'], {
+        duration: theme.transitions.duration.enteringScreen,
+        easing: theme.transitions.easing.sharp,
+      }),
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
   },
   avatar: {
-    width: `${theme.spacing.unit * 5}px`,
-    height: `${theme.spacing.unit * 5}px`,
-    borderRadius: 0,
     background: '#3ED58C',
+    borderRadius: 0,
     color: theme.palette.common.black,
-  },
-  bottom: {
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
-  },
-  content: {
-    padding: `${theme.spacing.unit * 3}px`,
+    height: `${theme.spacing.unit * 5}px`,
+    width: `${theme.spacing.unit * 5}px`,
   },
   divider: {
     backgroundColor: theme.palette.background.default,
   },
   drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
+    [theme.breakpoints.up('sm')]: {
+      flexShrink: 0,
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+    },
   },
-  drawerOpen: {
+  drawerPaper: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
     width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
   },
   drawerClose: {
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: `${theme.spacing.unit * 7}px`,
     [theme.breakpoints.up('sm')]: {
-      width: `${theme.spacing.unit * 9}px`,
+      overflowX: 'hidden',
+      transition: theme.transitions.create('width', {
+        duration: theme.transitions.duration.leavingScreen,
+        easing: theme.transitions.easing.sharp,
+      }),
+      width: (theme.spacing.unit * 9) + 1,
+    },
+  },
+  drawerOpen: {
+    [theme.breakpoints.up('sm')]: {
+      transition: theme.transitions.create('width', {
+        duration: theme.transitions.duration.enteringScreen,
+        easing: theme.transitions.easing.sharp,
+      }),
+      width: drawerWidth,
     },
   },
   hide: {
-    display: 'none',
-  },
-  item: {
-    opacity: 0.7,
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
   },
   list: {
-    color: theme.palette.common.white,
-    backgroundColor: theme.palette.common.black,
     height: '100%',
+  },
+  listItem: {
+    opacity: 0.7,
+    maxHeight: '48px',
+    padding: '16px',
+    marginTop: '16px',
+  },
+  'listItem-actived': {
+    opacity: 1,
   },
   listItemIcon: {
     color: theme.palette.common.white,
     marginLeft: theme.spacing.unit,
     opacity: 'inherit',
   },
-  menuButton: {
-    marginLeft: `${theme.spacing.unit * 8}px`,
+  logout: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
   },
-  menuButtonOpen: {
-    marginLeft: `${theme.spacing.unit * 1}px`,
+  main: {
+    flexGrow: 1,
+    minHeight: '100vh',
+    padding: `0 ${theme.spacing.unit * 15}px`,
+    [theme.breakpoints.only('xs')]: {
+      padding: `0 ${theme.spacing.unit * 2}px`,
+    },
+  },
+  menuBtn: {
+    marginRight: theme.spacing.unit * 2,
+    [theme.breakpoints.up('sm')]: {
+      marginRight: theme.spacing.unit * 3,
+    },
+  },
+  'menuBtn-close': {
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: 72,
+    },
   },
   primary: {
     color: 'inherit',
     fontWeight: 600,
     fontSize: '18px',
   },
-  profileBadge: {
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 2}px`,
-    backgroundColor: theme.palette.background.secondary,
-  },
   root: {
     display: 'flex',
-  },
-  signoutBtn: {
-    backgroundColor: theme.palette.common.black,
-    minHeight: '90px',
+    width: '100%',
   },
   secondary: {
     color: 'inherit',
     fontWeight: 'normal',
     fontSize: '14px',
   },
+  signOutBtn: {
+    backgroundColor: theme.palette.common.black,
+    minHeight: '90px',
+  },
   toolbar: {
-    background: theme.palette.common.black,
-    display: 'flex',
     alignItems: 'center',
+    display: 'flex',
     justifyContent: 'center',
-    padding: `${theme.spacing.unit * 2}`,
     ...theme.mixins.toolbar,
   },
-  
+  triangle: {
+    width: 0,
+    height: 0,
+    borderRight: '10px solid #f7f7f7',
+    borderTop: '10px solid transparent',
+    borderBottom: '10px solid transparent',
+    position: 'absolute',
+  },
+  'triangle-expanded': {
+    left: `calc(${drawerWidth}px - 11px)`,
+    transition: theme.transitions.create(['right', 'right'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  'triangle-collapsed': {
+    left: '62px',
+    transition: theme.transitions.create(['left', 'left'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
 });
 
-const nameToInitials = 'NA';
-const name = 'Nombre Apellido';
-const email = 'nombre.apellido@dominio.la';
-const itemsArray = ['Vacantes', 'Talento'];
-const iconsArray = [<DescriptionIcon />, <PersonIcon />];
-
+const nameToInitials = (name = '') => name.split(' ').reduce((memo, item) => {
+  if (item[0] && memo.length < 3) {
+    return memo + item[0].toUpperCase();
+  }
+  return memo;
+}, '');
 
 const SideBar = (props, theme) => {
-  const { classes, main, firebase } = props;
-  const [open, setOpen] = React.useState(false);
-  
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+  const {
+    classes,
+    container,
+    items,
+    logout,
+    main,
+    user: { name, email },
+  } = props;
+  const [isOpen, setIsOpen] = React.useState(true);
+  const [activedItem, setActivedItem] = React.useState(items[0].id);
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
+  const drawer = (
+    <>
+      <div className={classes.toolbar}>
+        { isOpen
+          ? <img src={logoWhite} alt="Logo" width="75%" />
+          : <img src={isotypeWhite} alt="Isotipo" width="75%" /> }
+      </div>
+      <Divider className={classes.divider} />
+      <ListItem className={classes.profileBadge}>
+        <Avatar className={classes.avatar} >{ nameToInitials(name) }</Avatar>
+        <ListItemText
+          classes={{ primary: classes.primary, secondary: classes.secondary }}
+          primary={ name }
+          secondary={ email }
+        />
+      </ListItem>
+      <Divider className={classes.divider} />
+      <List className={classes.list}>
+        {items.map(item => (
+          <ListItem
+            button
+            classes={{
+                root: classes.listItem,
+                selected: classes['listItem-actived'],
+            }}
+            key={item.id}
+            onClick={() => setActivedItem(item.id)}
+            selected={activedItem === item.id}
+          >
+            <ListItemIcon className={classes.listItemIcon}>{item.icon}</ListItemIcon>
+            <ListItemText
+              classes={{ primary: classes.primary }}
+              primary={item.text} />
+            {item.id === activedItem && (
+              <div
+                className={clsx(classes.triangle, {
+                  [`${classes['triangle-expanded']}`]: isOpen,
+                  [`${classes['triangle-collapsed']}`]: !isOpen,
+                })} />
+            )}
+          </ListItem>
+        ))}
+        <div className={classes.logout}>
+          <Divider className={classes.divider} />
+          <ListItem
+            button
+            className={classes.signOutBtn}
+            onClick={logout}
+          >
+            <ListItemIcon className={classes.listItemIcon}>
+              <ExitToAppIcon />
+            </ListItemIcon>
+            <ListItemText
+              classes={{ primary: classes.primary }}
+              primary="Cerrar Sesión"
+              style={{ paddingLeft: '8px' }}
+            />
+          </ListItem>
+        </div> 
+      </List>
+    </>
+  );
 
   return (
     <div className={classes.root}>
+      <CssBaseline />
       <AppBar
-        position="fixed"
-        elevation={0}
         className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
+          [classes.appBarShift]: isOpen,
         })}
+        elevation={0}
+        position="fixed"
       >
         <Toolbar>
           <IconButton
-            color="inherit"
             aria-label="open drawer"
-            onClick={open ? handleDrawerClose : handleDrawerOpen }
+            className={clsx(classes.menuBtn, !isOpen && classes['menuBtn-close'])}
+            color="inherit"
             edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.menuButtonOpen]: open,
-            })}
+            onClick={() => setIsOpen(!isOpen)}
           >
             <MenuIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
-      <Drawer
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}
-        classes={{
-          paper: clsx({
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          }),
-        }}
-      >
-        <div className={classes.toolbar}>
-          { open ? <img src={logoWhite} alt="Logo" width="75%" /> : <img src={isotypeWhite} alt="Isotipo" width="75%" /> }
-        </div>
-        <Divider />
-        <List className={classes.list}>
-          <ListItem className={classes.profileBadge}>
-            <Avatar className={classes.avatar} >
-              { nameToInitials }
-            </Avatar>
-            <ListItemText
-              classes={{ primary: classes.primary, secondary: classes.secondary }}
-              primary={ name }
-              secondary={ email }
-            />
-          </ListItem>
-          <Divider className={classes.divider} />
-          {itemsArray.map((text, index) => (
-            <ListItem
-              button
-              style={{ maxHeight: '48px', padding: '16px', marginTop: '16px' }}
-              key={text}
-              onClick
-            >
-              <ListItemIcon className={classes.listItemIcon}>
-                {iconsArray[index]}
-              </ListItemIcon>
-              <ListItemText
-                classes={{ primary: classes.primary }}
-                style={{ paddingLeft: '8px' }}
-                primary={text} />
-            </ListItem>
-          ))}
-          <div className={classes.bottom}>
-            <Divider className={classes.divider} />
-            <ListItem
-              button
-              className={classes.signoutBtn}
-              onClick={() => firebase.logout()}
-            >
-              <ListItemIcon className={classes.listItemIcon}>
-                <ExitToAppIcon />
-              </ListItemIcon>
-              <ListItemText
-                classes={{ primary: classes.primary }}
-                primary="Cerrar Sesión"
-                style={{ paddingLeft: '8px' }}
-              />
-            </ListItem> 
-          </div> 
-        </List>
-      </Drawer>
-      <main className={classes.content}>{main}</main>
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={'left'}
+            open={isOpen}
+            onClose={() => setIsOpen(!isOpen)}
+            classes={{
+              root: classes.hide,
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              root: clsx(classes.drawer, isOpen ? classes.drawerOpen : classes.drawerClose),
+              paper: clsx(classes.drawerPaper, {
+                [classes.drawerOpen]: isOpen,
+                [classes.drawerClose]: !isOpen,
+              }),
+            }}
+            open={isOpen}
+            variant="permanent"
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      <main className={classes.main}>
+        <div className={classes.toolbar} />
+        {main}
+      </main>
     </div>
   );
 }
 
 SideBar.propTypes = {
-  auth: PropTypes.shape({
-    displayName: PropTypes.string,
-    email: PropTypes.string.isRequired,
-  }).isRequired,
   classes: PropTypes.shape({
-    active: PropTypes.string.isRequired,
     appBar: PropTypes.string.isRequired,
     appBarShift: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
-    bottom: PropTypes.string.isRequired,
-    content: PropTypes.string.isRequired,
     divider: PropTypes.string.isRequired,
     drawer: PropTypes.string.isRequired,
-    drawerOpen: PropTypes.string.isRequired,
-    drawerClose: PropTypes.string.isRequired,
+    drawerPaper: PropTypes.string.isRequired,
     hide: PropTypes.string.isRequired,
-    item: PropTypes.string.isRequired,
     list: PropTypes.string.isRequired,
+    listItem: PropTypes.string.isRequired,
+    'listItem-actived': PropTypes.string.isRequired,
     listItemIcon: PropTypes.string.isRequired,
-    menuButton: PropTypes.string.isRequired,
-    menuButtonOpen: PropTypes.string.isRequired,
+    logout: PropTypes.string.isRequired,
+    main: PropTypes.string.isRequired,
+    menuBtn: PropTypes.string.isRequired,
+    'menuBtn-close': PropTypes.string.isRequired,
     primary: PropTypes.string.isRequired,
-    profileBadge: PropTypes.string.isRequired,
     root: PropTypes.string.isRequired,
-    signoutBtn: PropTypes.string.isRequired,
     secondary: PropTypes.string.isRequired,
+    signOutBtn: PropTypes.string.isRequired,
     toolbar: PropTypes.string.isRequired,
-  }).isRequired,   
-  firebase: PropTypes.shape({
-    logout: PropTypes.func.isRequired,
+    triangle: PropTypes.string.isRequired,
+    'triangle-expanded': PropTypes.string.isRequired,
+    'triangle-collapsed': PropTypes.string.isRequired,
   }).isRequired,
-  profile: PropTypes.shape({
-    name: PropTypes.string,
-    github: PropTypes.string,
-    roles: PropTypes.shape({
-      admin: PropTypes.bool,
-    }),
-  }),
-  main: PropTypes.node,
-};
-
-SideBar.defaultProps = {
-  profile: {},
-  itemsArray: [],
-  iconsArray: [],
 };
 
 export default withStyles(styles)(SideBar);
