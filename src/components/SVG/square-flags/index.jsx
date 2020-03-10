0395f3e5a61230984adefe3932fff1b267accd4c
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2020-03-10 16:51:13 -0500

    feat(SVG): Add square flags

---
+++ src/components/SVG/square-flags/index.jsx
@@ -0,0 +1,7 @@
+export { default as aqpFlag } from './peru.svg'; // Arequipa
+export { default as limFlag } from './peru.svg'; // Lima
+export { default as gdlFlag } from './mexico.svg'; // Guadalajara
+export { default as cdmxFlag } from './mexico.svg'; // Ciudad de México
+export { default as sclFlag } from './chile.svg'; // Santiago de Chile
+export { default as splFlag } from './brasil.svg'; // São Paulo
+export { default as bogFlag } from './colombia.svg'; // Bogotá
