0b6bba48745f937c475746dd1827ca1d99c7bbb1
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2020-03-10 17:16:26 -0500

    refactor(SVG): Update comments

--- src/components/SVG/flags/index.jsx
+++ src/components/SVG/flags/index.jsx
@@ -1,7 +1,7 @@
 export { default as aqpFlag } from './peru.svg'; // Arequipa
 export { default as limFlag } from './peru.svg'; // Lima
 export { default as gdlFlag } from './mexico.svg'; // Guadalajara
-export { default as cdmxFlag } from './mexico.svg'; // Ciudad de mexico
-export { default as sclFlag } from './chile.svg'; // Santiago del chile
+export { default as cdmxFlag } from './mexico.svg'; // Ciudad de México
+export { default as sclFlag } from './chile.svg'; // Santiago de Chile
 export { default as splFlag } from './brasil.svg'; // São Paulo
 export { default as bogFlag } from './colombia.svg'; // Bogotá
