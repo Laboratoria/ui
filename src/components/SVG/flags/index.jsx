041eb1d0a10bb5315c1e15c8d4f4a886017e6ecf
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2020-03-10 16:56:28 -0500

    refactor(SVG): Rename file of Brazil flag

--- src/components/SVG/flags/index.jsx
+++ src/components/SVG/flags/index.jsx
@@ -3,5 +3,5 @@ export { default as limFlag } from './peru.svg'; // Lima
 export { default as gdlFlag } from './mexico.svg'; // Guadalajara
 export { default as cdmxFlag } from './mexico.svg'; // Ciudad de mexico
 export { default as sclFlag } from './chile.svg'; // Santiago del chile
-export { default as splFlag } from './spl.svg'; // São Paulo
+export { default as splFlag } from './brasil.svg'; // São Paulo
 export { default as bogFlag } from './colombia.svg'; // Bogotá
