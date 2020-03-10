bd78064f71d203e5cea55cddd2cd462695292091
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2020-03-10 16:21:20 -0500

    feat(SVG): Export Colombia flag

--- src/components/SVG/flags/index.jsx
+++ src/components/SVG/flags/index.jsx
@@ -4,3 +4,4 @@ export { default as gdlFlag } from './mexico.svg'; // Guadalajara
 export { default as cdmxFlag } from './mexico.svg'; // Ciudad de mexico
 export { default as sclFlag } from './chile.svg'; // Santiago del chile
 export { default as splFlag } from './spl.svg'; // São Paulo
+export { default as bogFlag } from './colombia.svg'; // Bogotá
