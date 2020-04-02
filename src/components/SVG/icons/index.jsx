c377b2fe6a29b9f155171646c8c73e1f3f4714d8
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2020-04-02 11:53:25 -0500

    feat(SVG): Export grey icons

--- src/components/SVG/icons/index.jsx
+++ src/components/SVG/icons/index.jsx
@@ -1,3 +1,8 @@
 export { default as iconGithub } from './github.svg'; // Github
 export { default as iconLinkedin } from './linkedin.svg'; // Linkedin
 export { default as iconPortfolio } from './portfolio.svg'; // Portfolio
+
+// Grey icons
+export { default as greyIconGithub } from './github-grey.svg'; // Github
+export { default as greyIconLink } from './link-grey.svg'; // Link
+export { default as greyIconLinkedin } from './linkedin-grey.svg'; // Linkedin
