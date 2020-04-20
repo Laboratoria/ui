c76f412ebcb66a211eeb149a0f8542f7b845feac
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2020-04-20 08:24:06 -0500

    refactor(storybook): Update primary contained button

--- src/components/Buttons/index.stories.js
+++ src/components/Buttons/index.stories.js
@@ -4,5 +4,12 @@ import { storiesOf } from '@storybook/react';
 import Button from './index';
 
 storiesOf('Buttons', module)
-  .add('Primary as a link', () => <PrimaryButton tag="a"> Click me </PrimaryButton>)
-  .add('Primary as a button', () => <PrimaryButton tag="button"> Click me </PrimaryButton>);
+  .add('Primary contained button', () => (
+    <>
+      <Button size="large" style={{ marginBottom: 16 }}>Publica una vacante</Button>
+      <br />
+      <Button style={{ marginBottom: 16 }}>Publica una vacante</Button>
+      <br />
+      <Button size="small">Publica una vacante</Button>
+    </>
+  ));
