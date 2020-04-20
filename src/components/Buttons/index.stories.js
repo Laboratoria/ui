369d382e89da6009a7d92e229f2601755a39ea40
MeryCardenas23 <mery.cardenas.perez@gmail.com>
2020-04-20 08:25:35 -0500

    feat(storybook): Add secondary contained button

--- src/components/Buttons/index.stories.js
+++ src/components/Buttons/index.stories.js
@@ -12,4 +12,13 @@ storiesOf('Buttons', module)
       <br />
       <Button size="small">Publica una vacante</Button>
     </>
+  ))
+  .add('Secondary contained button', () => (
+    <>
+      <Button size="large" style={{ marginBottom: 16 }} color="secondary">Regístrate</Button>
+      <br />
+      <Button style={{ marginBottom: 16 }} color="secondary">Regístrate</Button>
+      <br />
+      <Button size="small" color="secondary">Regístrate</Button>
+    </>
   ));
